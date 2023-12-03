400f0b25bf513d92c24325e0d988c93a55c777bd
Victor Campos <62899837+campossvictor@users.noreply.github.com>
2023-12-03 06:56:55 +0000

    Primeiro Commit

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1>Primeiro mês juntos(mas longe rs). Mesmo estando um mês do seu lado, parece que foram meses e meses. Te amo pézinho de chumbo, cuidado com a estrada hoje ein! </h1> 
+}
+
+export default Home
