3ca7f827e61deb270bb7c23151a0dd8b1a7af519
Victor Campos <62899837+campossvictor@users.noreply.github.com>
2023-12-04 20:33:18 +0000

    adiciona o script do `lint:check` e do `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Mari, te amo muito</h1> 
+function Home() {
+  return <h1>Mari, te amo muito</h1>;
 }
 
-export default Home
+function teste() {
+  console.log("teste");
+  return;
+}
+
+export default Home;
